2b2ce36960878a253b4b9aa2ef9a7e8327abbe16
cl9nt <muthunguclintn@gmail.com>
2023-07-20 10:42:26 +0300

    updated on 20/07/23

---
+++ Leetcode-Interview-Questions/Rotate-Image/rotate.py
@@ -0,0 +1,13 @@
+def rotate(matrix):
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+
+    return matrix
